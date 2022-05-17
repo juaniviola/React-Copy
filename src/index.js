947453a3bee87fac99738761ffdeb58887eaf200
juaniviola <juaniviola1297@gmail.com>
2022-05-17 18:01:03 -0300

    performUnitOfWork functionality

--- src/index.js
+++ src/index.js
@@ -24,23 +24,92 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
-  const dom = element.type === 'TEXT_ELEMENT'
+function createDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT'
     ? document.createTextNode("")
     : document.createElement(element.type);
 
   const isProp = (key) => key !== 'children';
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProp)
     .forEach((propName) => {
-      dom[propName] = element.props[propName];
+      dom[propName] = fiber.props[propName];
     });
 
-  element.props.children.forEach((child) => {
-    return render(child, dom);
-  });
+  return dom;
+}
 
-  container.appendChild(dom);
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0, prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (!index) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index += 1;
+  }
+
+  // search next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const ReactCopy = {
