cd880f2781cdf7c8d563fb9d86d2a040d6961cb4
juaniviola <juaniviola1297@gmail.com>
2022-05-17 17:48:04 -0300

    simple render version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** @jsxRuntime classic */
+/** @jsx ReactCopy.createElement */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object'
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  };
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  const dom = element.type === 'TEXT_ELEMENT'
+    ? document.createTextNode("")
+    : document.createElement(element.type);
+
+  const isProp = (key) => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProp)
+    .forEach((propName) => {
+      dom[propName] = element.props[propName];
+    });
+
+  element.props.children.forEach((child) => {
+    return render(child, dom);
+  });
+
+  container.appendChild(dom);
+}
+
+const ReactCopy = {
+  createElement,
+  render,
+};
+
+const element = (
+  <div id="foo">
+    <h1>hola</h1>
+    <a href="https://google.com/">something</a>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+ReactCopy.render(element, container);
